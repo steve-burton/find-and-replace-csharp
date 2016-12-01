2e7f9618542f53a26fef616495cb0fb252fa0a4c
Levi Bibo and Steve Burton <levi+steve@>
2016-11-30 16:26:51 -0800

    add form and output on front end.

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -10,7 +10,19 @@ namespace FindReplace
 		{
 			Get["/"] = _ =>
 			{
-				return View["index.cshtml"];
+				Dictionary<string, string> objects = new Dictionary<string, string>();
+				objects["HasSentence"] = "false";
+				return View["index.cshtml", objects];
+			};
+			Post["/"] = _ =>
+			{
+				Dictionary<string, string> objects = new Dictionary<string, string>();
+				objects["HasSentence"] = "true";
+				string userString = Request.Form["userString"];
+				string findWord = Request.Form["findWord"];
+				string replaceWord = Request.Form["replaceWord"];
+				objects["ReplacedSentence"] = UserInput.FindAndReplace(userString, findWord, replaceWord);
+				return View["index.cshtml", objects];
 			};
 		}
 	}
